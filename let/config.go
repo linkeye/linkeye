bc0ea0c3b3cc4a4c73a0d79eb35b18542806ec68
guosanbin <guosanbin@linkeye.com>
2018-12-03 13:58:51 +0800

    modify syncMode: fast->Full

--- let/config.go
+++ let/config.go
@@ -18,7 +18,7 @@ import (
 
 // DefaultConfig contains default settings for use on the Ethereum main net.
 var DefaultConfig = Config{
-	SyncMode: downloader.FastSync,
+	SyncMode: downloader.FullSync,
 	//NetworkId:     1,
 	NetworkId:     8054,
 	LightPeers:    100,
